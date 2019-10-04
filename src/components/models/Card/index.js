f2cb95cea8c2cf6f8efb10f1692bc7bd304abb8d
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-04 11:28:52 -0300

    className-and-key

--- src/components/models/Card/index.js
+++ src/components/models/Card/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Card = ({ header, children }) => (
-  <div class="card">
+const Card = ({ header, children, className, id }) => (
+  <div className={ className } key={id}>
+      <div class="card">
     <div class="card-header">{ header }</div>
     <div class="card-body">
       { children }
     </div>
+  </div>
   </div>
 );
 

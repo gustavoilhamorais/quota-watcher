39eff82fe4f9edd3b9e5d5e5c906a10190530a8b
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-04 11:15:25 -0300

    component-abstraction

---
+++ src/components/Login/index.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
+const Main = props => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(0);
+  const [redirect, setRedirect] = useState(0);
+
+  const handleSubmit = form => {
+    form.preventDefault();
+    if (rememberMe)
+      localStorage.setItem(
+        "@QuotaWatcher#SavedAccount",
+        JSON.stringify({ email, password })
+      );
+    if (email && password) setRedirect(1);
+  };
+
+  const handleRememberMe = () => {
+    if (rememberMe) setRememberMe(0);
+    else setRememberMe(1);
+  };
+
+  const checkSavedAccount = () => {
+    const savedAccount = localStorage.getItem("@QuotaWatcher#SavedAccount");
+    if (savedAccount) {
+      setEmail(savedAccount.account);
+      setPassword(savedAccount.password);
+    }
+  };
+
+  useEffect(() => {
+    checkSavedAccount();
+  }, []);
+
+  return (
+    <div className="col col-lg-5">
+      {redirect ? (
+        <Redirect
+          to={{ pathname: "/dashboard", state: { from: props.location } }}
+        />
+      ) : (
+        <></>
+      )}
+      <form className="form-signin" onSubmit={form => handleSubmit(form)}>
+        <img
+          className="mb-4"
+          src="../assets/log-in.svg"
+          alt="Logo"
+          width="100"
+          height="100"
+        />
+        <h1 className="h3 mb-3 font-weight-normal">Faça login</h1>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              seu@email.com
+            </span>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Usuário"
+            aria-label="Usuário"
+            aria-describedby="basic-addon1"
+            value={email}
+            onChange={input => setEmail(input.target.value)}
+          />
+        </div>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              <b>* * * * * * * * * * *</b>
+            </span>
+          </div>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Senha"
+            aria-label="Senha"
+            aria-describedby="basic-addon1"
+            value={password}
+            onChange={input => setPassword(input.target.value)}
+          />
+        </div>
+        <label>
+          <input
+            type="checkbox"
+            value={rememberMe}
+            onChange={() => handleRememberMe()}
+          />{" "}
+          Lembrar de mim
+        </label>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+          Entrar
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2019</p>
+      </form>
+    </div>
+  );
+};
+
+export default Main;

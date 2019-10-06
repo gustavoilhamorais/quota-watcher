670591e991dd9a7f187f48504658341a7f476b4c
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-06 11:35:48 -0300

    session-time-handler

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,3 +1,14 @@
 export default function authenticated() {
-    return true;
+    const auth = JSON.parse(localStorage.getItem("@QuotaWatcher#LastLogin"));
+    if (auth) {
+        const lastLogin = new Date(auth.date);
+        const minutes = lastLogin.getMinutes();
+        const hours = lastLogin.getHours();
+        const nowMinutes = new Date().getMinutes();
+        const nowHours = new Date().getHours();
+        if (hours === nowHours) {
+            if (minutes - nowMinutes > 45) return false;
+            else return true;
+        } else return false;
+    } else return false
 }

d9e3c3ac9471d8db4b5e51fe33dcf2f1dcdc35a7
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-06 11:34:47 -0300

    loading-bar

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './index.css';
+import React, {useState, useEffect} from "react";
 
-const Loading = ({ render, status }) => (
-    status
-        ? render
-        : <div className="loader"></div>
-);
+const Loading = ({ loading }) => {
+  const [load, setLoad] = useState(0);
+
+  const loader = () => {
+    for (let index = 0; index < 100; index++) {
+      setInterval(() => {
+        setLoad(index);
+      }, 1000);
+      clearTimeout();
+    }
+  }
+
+  useEffect(() => {
+    if(loading) loader();
+  }, []);
+
+  return (
+    <div className="progress">
+      <div
+        className="progress-bar progress-bar-striped progress-bar-animated"
+        role="progressbar"
+        aria-valuenow={load}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        style={{ width: "100%" }}
+      ></div>
+    </div>
+  );
+};
 
 export default Loading;

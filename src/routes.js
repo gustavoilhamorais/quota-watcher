cdc3c9a6d62fe2dd7d3cffb3dcb5077bde4071a9
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-06 11:34:21 -0300

    navbar-logout-stocks-crypto

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import { Home, Login, Register, Dashboard, Error } from './pages';
+import { Home, Login, Register, Error, Stock, Crypto } from './pages';
 import authenticated from './helpers/auth';
+import logout from './helpers/logout';
+import Navbar from './components/Navbar';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={props => (
     authenticated() 
-      ? <Component {...props} />
+      ? (
+      <>
+      <Navbar />
+      <Component {...props} />
+      </>
+      )
       : <Redirect to={{ pathname: '/login', state: {from: props.location} }} />
   )} />
 );
+
+const Logout = () => {
+  logout();
+  return <Redirect to={{ pathname: '/login'}} />
+}
 
 const Routes = () => {
   return (
@@ -19,7 +31,9 @@ const Routes = () => {
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/register" component={Register}/>
-        <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/logout" component={Logout}/>
+        <PrivateRoute exact path="/stocks" component={Stock}/>
+        <PrivateRoute exact path="/crypto" component={Crypto}/>
         <Route path="*" component={Error}/>
       </Switch>
     </BrowserRouter>

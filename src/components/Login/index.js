1d6fce1d075ae183e13e8adfbf92521549683482
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-06 11:36:41 -0300

    validation-and-remember

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,14 +8,27 @@ const Main = props => {
   const [rememberMe, setRememberMe] = useState(0);
   const [redirect, setRedirect] = useState(0);
 
+  const validateLogin = () => {
+    const createdAccounts = JSON.parse(localStorage.getItem("@QuotaWatcher#CreatedAccounts"));
+    const isRegistered = createdAccounts[email];
+    if (isRegistered) {
+      if (createdAccounts[email].password === password) return true;
+      else return 'Senha inválida';
+    } else return 'Conta não existe';
+  }
+
   const handleSubmit = form => {
     form.preventDefault();
-    if (rememberMe)
-      localStorage.setItem(
-        "@QuotaWatcher#SavedAccount",
-        JSON.stringify({ email, password })
-      );
-    if (email && password) setRedirect(1);
+    if (rememberMe) localStorage.setItem("@QuotaWatcher#SavedAccount", JSON.stringify({ email, password }));
+    if (email && password) {
+      const validation = validateLogin();
+      if (validation === true) {
+        localStorage.setItem("@QuotaWatcher#LastLogin", JSON.stringify({
+          email, date: Date.now()
+        }));
+        setRedirect(1);
+      } else window.alert(validation);
+    }
   };
 
   const handleRememberMe = () => {
@@ -37,20 +50,8 @@ const Main = props => {
 
   return (
     <div className="container h-100 d-flex justify-content-center mt-5">
-      <Card
-        id="login"
-        className="col col-lg-5"
-        header={
-          <p className="h3 mb-3 font-weight-normal text-center">Login</p>
-        }
-      >
-        {redirect ? (
-          <Redirect
-            to={{ pathname: "/dashboard", state: { from: props.location } }}
-          />
-        ) : (
-          <></>
-        )}
+      <Card id="login" className="col col-lg-5" header={<p className="h3 mb-3 font-weight-normal text-center">Login</p>}>
+        {redirect ? <Redirect to={{ pathname: "/stocks", state: { from: props.location } }} /> : <></>}
         <form className="form-signin" onSubmit={form => handleSubmit(form)}>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
@@ -85,17 +86,13 @@ const Main = props => {
             />
           </div>
           <label>
-            <input
-              type="checkbox"
-              value={rememberMe}
-              onChange={() => handleRememberMe()}
-            />{" "}
-            Lembrar de mim
+            <input type="checkbox" value={rememberMe} onChange={() => handleRememberMe()} /> Lembrar de mim
           </label>
           <button className="btn btn-lg btn-primary btn-block mb-3 mt-2" type="submit">
             Entrar
           </button>
-          <span>Não possui uma conta? </span><Link to="/register">Cadastre-se.</Link>
+          <span>Não possui uma conta? </span>
+          <Link to="/register">Cadastre-se.</Link>
           <p className="mt-5 mb-3 text-muted text-center">&copy; 2019</p>
         </form>
       </Card>

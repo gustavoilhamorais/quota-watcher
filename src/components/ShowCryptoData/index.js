66577942f4b250b92c476d9884b8154d83853f38
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-10-06 16:16:43 -0300

    nan-removed-from-chart

--- src/components/ShowCryptoData/index.js
+++ src/components/ShowCryptoData/index.js
@@ -9,7 +9,7 @@ const ShowCryptoData = ({ data, selected }) => {
         id={selected}
         title={data.name || "Selecione uma criptomoeda"}
         series={[
-          { data: [{y: data.buy, color: "#007bff"}, { y: data.variation, color: data.variation > 0 ? "green" : "red" }, {y: data.sell, color: "#ffc107"}] }
+          { data: [{y: data.buy ? data.buy : 0, color: "#007bff"}, { y: data.variation ? data.variation : 0, color: data.variation > 0 ? "green" : "red" }, {y: data.sell ? data.sell : 0, color: "#ffc107"}] }
         ]}
       />
       <CryptoDataCard className="col col-lg-10 mt-5 mb-5" data={data} />
